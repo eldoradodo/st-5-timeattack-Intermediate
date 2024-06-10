ebc849b5f27310ad41f8d4b63d48207dd28cbe57
eldoradodo <rla1eh2dus3@naver.com>
2024-06-10 15:37:20 +0900

    계산기 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,47 @@
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
+import { useState } from "react";
+import {add, substract } from "
 
 // TODO: 계산결과를 redux를 이용한 전역상태로 관리해 보세요.
 function App() {
+
+   const [inputValue, setInputValue] = useState(0);
+   const result = useSelector((state) => state.result);
+   const dispatch = useDispatch();
+
+  const add = () => {
+    dispatch(add((Number(inputValue)));
+    setInputValue(0);
+  };
+
+  const sub = () => {
+    dispatch({
+      
+    });
+  }
+
   return (
     <div className="App">
       <h1>덧셈과 뺄셈이 가능한 앱 만들기</h1>
       <div>
-        <input /> 만큼을 <button>더할게요</button> <button>뺄게요</button>
+        <input 
+        type="number"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value}
+        /> 
+        만큼을 
+        <button onClick={add}>
+          더할게요
+          </button> 
+          <button onClick={sub}>
+            뺄게요
+            </button>
       </div>
       <hr />
       <div>
         <h3>결과</h3>
-        <p>0</p>
+        <p>{}</p>
       </div>
     </div>
   );
